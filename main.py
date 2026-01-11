84e7bbc8f8a1587400931610d2853e280180e31d
Void <stas050595@gmail.com>
2026-01-11 19:53:31 +0100

    Added robots.txt

--- main.py
+++ main.py
@@ -169,5 +169,9 @@ def IncrementUsage():
 def sitemap():
     return send_from_directory('static', 'sitemap.xml')
 
+@app.route('/robots.txt')
+def robots():
+    return send_from_directory('static' , 'robots.txt')
+
 if __name__ == "__main__":
     app.run()
